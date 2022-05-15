52236eec654ca90d1e4e5aeb03b836073a486d89
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-05-15 09:48:59 +0100

    Rebuilt Cython and numpy

--- repos/build_cython.sh
+++ repos/build_cython.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-tmp_dir=$(mktemp -d -t build311-XXXXXX)
-virtualenv "$tmp_dir" --python python3.11
-source "$tmp_dir/bin/activate"
-python3 -m pip install setuptools wheel
-python3 -m pip wheel ./cython --no-build-isolation
-
-rm -rf "$tmp_dir"
+python3 -m pip wheel git+https://github.com/cython/cython
--- repos/build_numpy.sh
+++ repos/build_numpy.sh
@@ -4,9 +4,10 @@ virtualenv "$tmp_dir" --python python3.11
 source "$tmp_dir/bin/activate"
 python3 -m pip install "setuptools<60" wheel
 python3 -m pip install Cython-3.0.0a10-cp311-cp311-linux_x86_64.whl
-cd numpy
-git submodule update --init
-cd ..
-python3 -m pip wheel ./numpy --no-build-isolation
+# cd numpy
+# git submodule update --init
+# cd ..
+# python3 -m pip wheel ./numpy --no-build-isolation -vv
+python3 -m pip wheel git+https://github.com/numpy/numpy --no-build-isolation -vv
 
 rm -rf "$tmp_dir"
